48e87b94b6dad3daa17f0b923c9e10253afd4550
Yaroslav <35110747+KurumiFT@users.noreply.github.com>
2024-07-15 22:58:43 +0300

    Add project schema

---
+++ backend/src/modules/projects/schemas/project.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { Document } from "mongoose";
+import { User } from "src/modules/users/schemas/user.schema";
+
+@Schema()
+export class Project {
+    @Prop({type: String, required: true})
+    name: string
+
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true})
+    owner: mongoose.Types.ObjectId | User
+
+    @Prop({type: String, required: true})
+    apiKey: string
+
+    @Prop({type: Date, required: true})
+    timestamp: Date
+}
+
+export type ProjectDocumet = Project & Document
+export const ProjectSchema = SchemaFactory.createForClass(Project) 
