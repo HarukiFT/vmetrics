ac40e1dff1b6851f3c51e6242950a0c78296c413
Haruki <aratashiharuki@gmail.com>
2024-07-16 02:21:42 +0300

    Add side menu, login footer, profile footer

---
+++ frontend/src/components/SideMenu/Pages.tsx
@@ -0,0 +1,16 @@
+import LogsIcon from '@mui/icons-material/ClearAll';
+import ProjectsIcon from '@mui/icons-material/FolderCopy';
+
+export const pages = [   
+    {
+        icon: ProjectsIcon,
+        href: '/projects',
+        label: 'Проекты'
+    },
+
+    {
+        icon: LogsIcon,
+        href: '/logs',
+        label: 'Логи'
+    },
+]
--- frontend/src/components/SideMenu/SideMenu.tsx
+++ frontend/src/components/SideMenu/SideMenu.tsx
@@ -1,18 +1,69 @@
-import { Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
+import { Avatar, Box, Button, Container, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, SvgIconTypeMap, Toolbar, Tooltip, styled } from "@mui/material"
+import { Link } from "react-router-dom"
+import { pages } from "./Pages"
+import { OverridableComponent } from "@mui/material/OverridableComponent"
+
+const StyledLink = styled(Link)((t) => ({
+    textDecoration: 'none',
+    color: t.theme.palette.text.primary,
+    width: '100%',
+}))
+
+const LoginFooter = () => {
+    return (
+       <Box width={1} px={3} display={"flex"} alignItems={'center'} justifyContent={"center"}>
+            <Button variant="text" fullWidth>
+                <StyledLink to={'/login'}>Войти</StyledLink>
+            </Button>
+       </Box> 
+    )
+}
+
+const ProfileFooter = () => {
+    return (
+       <Box width={1} px={3} display={"flex"} alignItems={'center'} justifyContent={"center"}>
+            <Tooltip title="Профиль">
+                <IconButton>
+                    <Avatar  sx={{
+                        backgroundColor: 'primary.dark'
+                    }}>
+
+                    </Avatar>
+                </IconButton>
+            </Tooltip>
+       </Box> 
+    )
+}
 
 export default () => {
     return (
-        <Drawer variant="persistent" open={true}>
-            <List>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
+        <Drawer sx={{
+            '& .MuiDrawer-paper': {
+                py: 2
+            }
+        }} variant="permanent">
+            <Box flexGrow={1}>
+                <List>
+                    {
+                        pages.map(page => {
+                            return (
+                                <ListItem>
+                                    <StyledLink to={page.href}>
+                                        <ListItemButton>
+                                            <ListItemIcon>
+                                                <page.icon/>
+                                            </ListItemIcon>
+                                            <ListItemText>{page.label}</ListItemText>
+                                        </ListItemButton>
+                                    </StyledLink>
+                                </ListItem>
+                            )
+                        })
+                    }
+                </List>
+            </Box>
 
-                        </ListItemIcon>
-                        <ListItemText>First item</ListItemText>
-                    </ListItemButton>
-                </ListItem>
-            </List>
+            <LoginFooter/>
         </Drawer>
     )
 }
