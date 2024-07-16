ffd4b0bd927a28555618a3633f5c588775812dd9
Haruki <aratashiharuki@gmail.com>
2024-07-16 16:12:20 +0300

    Add auth/info GET endpoint

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,12 +1,15 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 import { UserPayload } from '../../shared/interfaces/user-payload.interface';
 import { JwtService } from '@nestjs/jwt';
+import { AuthGuard } from './guards/auth.guard';
+import { UserData } from './interfaces/user-data.interface';
+import { UsersService } from '../users/users.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService, private readonly jwtService: JwtService) {}
+    constructor(private readonly authService: AuthService, private readonly jwtService: JwtService, private readonly usersService: UsersService) {}
 
     @Post('/auth')
     async auth(@Body() authDto: AuthDto) {
@@ -24,6 +27,18 @@ export class AuthController {
 
         return {
             token
+        }
+    }
+
+    @Get('/info')
+    @UseGuards(AuthGuard)
+    async getUserInfo(@Request() request: any): Promise<UserData> {
+        const userId = request.user._id
+        const userDocument = await this.usersService.getUserById(userId)
+
+        return {
+            _id: userDocument.id,
+            username: userDocument.username
         }
     }
 }
