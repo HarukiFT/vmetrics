eaced80a940e888c75f9bb70f45d1d8a21eea2f8
Haruki <aratashiharuki@gmail.com>
2024-07-18 13:22:03 +0300

    Add get project by id endpoint

--- backend/src/modules/projects/projects.controller.ts
+++ backend/src/modules/projects/projects.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, UseGuards, Request, Body, Get } from '@nestjs/common'
 import { ProjectsService } from './projects.service';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { CreateProjectDto } from './dto/create-project.dto';
+import { ProjectGuard } from './guards/project.guard';
 
 @Controller('projects')
 export class ProjectsController {
@@ -11,6 +12,14 @@ export class ProjectsController {
     @UseGuards(AuthGuard)
     async createProject(@Request() request: any, @Body() createProjectDto: CreateProjectDto) {
         await this.projectsService.createProject(request.user, createProjectDto)
+    }
+
+    @Get('/get')
+    @UseGuards(AuthGuard, ProjectGuard)
+    async getProject(@Request() request: any) {
+        const projectId = request.projectId
+
+        return await this.projectsService.getProject(projectId)
     }
 
     @Get('/fetch')
