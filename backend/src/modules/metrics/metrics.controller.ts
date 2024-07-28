bfb59d437869bc7f3401a305b52dd8d750aac51d
Haruki <aratashiharuki@gmail.com>
2024-07-28 19:38:34 +0300

    Add create metrics logic

--- backend/src/modules/metrics/metrics.controller.ts
+++ backend/src/modules/metrics/metrics.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { MetricsService } from './metrics.service';
+import { ApiGuard } from '../projects/guards/api.guard';
+import { CreateMetricDto } from './dto/create-metric.dto';
+import { CreateMetricsDto } from './dto/create-metrics.dto';
 
 @Controller('metrics')
-export class MetricsController {}
+export class MetricsController {
+    constructor(private readonly metricsService: MetricsService) {}
+
+    @Post('/create')
+    @UseGuards(ApiGuard)
+    async createMetric(@Body() createMetricDto: CreateMetricDto, @Request() request: any) {
+        await this.metricsService.createMetric(request.projectId, createMetricDto)
+    }
+
+    @Post('/mcreate')
+    @UseGuards(ApiGuard)
+    async createMetrics(@Body() createMetricsDto: CreateMetricsDto, @Request() request: any) {
+        for (let dto of createMetricsDto.payload) {
+            await this.metricsService.createMetric(request.projectId, dto)
+        }
+    }
+}
--- backend/src/modules/metrics/metrics.service.ts
+++ backend/src/modules/metrics/metrics.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import mongoose, { Model } from 'mongoose';
+import { MetricDocument } from './schemas/metric.schema';
+import { CreateMetricDto } from './dto/create-metric.dto';
 
 @Injectable()
-export class MetricsService {}
+export class MetricsService {
+    constructor(@InjectModel('Metric') private readonly metricsModel: Model<MetricDocument>) {}
+
+    async createMetric(projectId: string, createMetricDto: CreateMetricDto): Promise<undefined> {
+        const projectOID = mongoose.Types.ObjectId.createFromHexString(projectId)
+        
+        const timestampDate = new Date(createMetricDto.timestamp)
+        
+        await new this.metricsModel({
+            timestamp: timestampDate, 
+            value: createMetricDto.value, 
+            project: projectOID, 
+            metadata: createMetricDto.clarification ? { clarification: createMetricDto.clarification } : undefined})
+            .save()
+    }
+}
